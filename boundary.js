4787cce56fdbb5a538481bcc473fa9cee0ffef04
Alex S <qwshao@uwaterloo.ca>
2021-12-18 03:29:19 -0500

    drew boundary

---
+++ boundary.js
@@ -0,0 +1,15 @@
+const { _findLine } = require("yt-search");
+
+class Boundary {
+
+    constructor(x1, y1, x2, y2) {
+        this.a = createVector(x1, y1);
+        this.b = createVector(x2, y2);
+    }
+
+    // draw the 'line' formed by the points that acts as a boundary
+    show() {
+        stroke(255);
+        line(this.a.x, this.a.y, this.b.x, this.b.y);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,15 @@
+let height = 400;
+let width = 400;
+
+let boundary;
+
+function setup() {
+
+    createCanvas(width, height);
+    boundary = new Boundary(300, 100, 300, 300);
+}
+
+function draw() {
+    background(0);
+    b.show();
+}
